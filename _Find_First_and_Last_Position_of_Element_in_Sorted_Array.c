5c157d6207eac0461595bfc4aff2cd1bf5ca5388
gvsaisurya02 <98895387+gvsaisurya02@users.noreply.github.com>
2022-10-27 12:15:45 +0530

    #include<stdio.h> int main() {     int a[50],n,t,i,flag=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     scanf("%d",&t);     for(i=0;i<n;i++)     {         if(a[i]==t)         {             printf("%d ",i);             flag=1;             break;         }     }     if(flag==0)     {         printf("-1 ");     }     for(i=n-1;i>=0;i--)     {         flag=0;         if(a[i]==t)         {             printf("%d",i);             flag=1;             break;         }     }     if(flag==0)     {         printf("-1 ");     }     return 0; }

---
+++ _Find_First_and_Last_Position_of_Element_in_Sorted_Array.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+    int a[50],n,t,i,flag=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    scanf("%d",&t);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==t)
+        {
+            printf("%d ",i);
+            flag=1;
+            break;
+        }
+    }
+    if(flag==0)
+    {
+        printf("-1 ");
+    }
+    for(i=n-1;i>=0;i--)
+    {
+        flag=0;
+        if(a[i]==t)
+        {
+            printf("%d",i);
+            flag=1;
+            break;
+        }
+    }
+    if(flag==0)
+    {
+        printf("-1 ");
+    }
+    return 0;
+}
